54a0b583b642ea92efcba40530245ed7fc41545d
Pan153001 <s6506021621021@email.kmutnb.ac.th>
2024-02-14 11:05:42 +0700

    commit

--- AxiosNodeHtml.js
+++ AxiosNodeHtml.js
@@ -4,9 +4,9 @@ var bodyParser = require('body-parser');
 const path = require("path");
 const app = express();
 
-const base_url = "https://node57018-pan-noderest.proen.app.ruk-com.cloud";
+const base_url = "http://node57018-pan-noderest.proen.app.ruk-com.cloud";
 
-app.set("views" , path.json(__dirname,'/public/views'));
+app.set("views" , path.join(__dirname,'/public/views'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));

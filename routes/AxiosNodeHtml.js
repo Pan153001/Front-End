9b8ae2936a4398d04264a8fee6fd52c660de3c8c
Pan153001 <s6506021621021@email.kmutnb.ac.th>
2024-02-14 10:46:26 +0700

    commit

--- routes/AxiosNodeHtml.js
+++ routes/AxiosNodeHtml.js
@@ -11,6 +11,8 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(express.static(__dirname + '/public/views'));
+
 app.get("/", async (req, res) => {
     try {
         const response = await axios.get(base_url + '/books');

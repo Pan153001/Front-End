0090505dfeb41064baf3a16d9c00fcf8d2f95b25
Pan153001 <kumakumakumaBEAR01>
2024-02-21 00:17:58 +0700

    commit

--- AxiosNodeHtml.js
+++ AxiosNodeHtml.js
@@ -6,7 +6,7 @@ const app = express();
 
 const base_url = "http://node57018-pan-noderest.proen.app.ruk-com.cloud";
 
-app.set("views" , path.join(__dirname,'/public/views'));
+app.set("views" , path.join(__dirname, "/public/views"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
